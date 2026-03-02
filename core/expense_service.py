5b5858fea21dd77fcc876e0ae247aecd3884ecab
Elias Marin <elias.marin101@gmail.com>
2026-03-02 18:36:04 +0100

    Fix: Add type annotation for totals in total_by_month

--- core/expense_service.py
+++ core/expense_service.py
@@ -77,7 +77,7 @@ class ExpenseService:
         return sum(gasto.amount for gasto in gastos)
 
     def total_by_month(self) -> dict[str, float]:
-        totals = defaultdict(float)
+        totals: dict[str, float] = defaultdict(float)
 
         for expense in self._repository.list_all():
             key = expense.expense_date.strftime("%Y-%m")
